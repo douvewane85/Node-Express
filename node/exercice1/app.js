b2efb8340616edd98ccc9e775ff19b4b31e62dfc
douvewane <douvewane85@gmail.com>
2025-11-21 09:42:22 +0000

    Exercice-1

---
+++ node/exercice1/app.js
@@ -0,0 +1,11 @@
+
+caculatrice ={
+      add:( a, b)=> a+b,
+      soustraction:( a, b)=> a-b,
+      multtiplication:( a, b)=> a*b,
+}
+
+console.log(caculatrice.add(10,11));
+console.log(caculatrice.soustraction(10,11));
+console.log(caculatrice.multtiplication(10,11));
+
